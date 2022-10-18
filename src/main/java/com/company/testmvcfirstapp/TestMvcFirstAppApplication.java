06f1732271d12bcc78e2339698ad9c7693ffae72
artemy-zim <art.zim228@yandex.ru>
2022-10-19 04:14:15 +0700

    Initial commit

---
+++ src/main/java/com/company/testmvcfirstapp/TestMvcFirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.company.testmvcfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMvcFirstAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMvcFirstAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/testmvcfirstapp/TestMvcFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.testmvcfirstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMvcFirstAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
